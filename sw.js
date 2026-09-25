e1ede8a1056f6d81171ad242a1189ae6d7a6e965
Alan <alanclebersantana@gmail.com>
2026-09-25 08:47:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker: guarda só os arquivos do app. Os dados (Apps Script) sempre vão pela rede.
-const CACHE = 'mp-emissoes-v5';
+const CACHE = 'mp-emissoes-v7';
 const SHELL = ['./', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png', './icons/favicon-32.png'];
 
